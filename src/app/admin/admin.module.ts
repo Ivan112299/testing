cc39442c5484db374e5337edb52337464a738f5d
Ivan Kozin <Ivan.kozin@zyfra.com>
2023-04-18 15:13:09 +0300

    add and setting text-editor

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { CreateComponent } from './components/create/create.component';
 import { LessonsComponent } from './components/lessons/lessons.component';
 import { AuthGuard } from '../services/auth.guard';
+import { NgxEditorModule } from 'ngx-editor';
 
 @NgModule({
   declarations: [
@@ -19,9 +20,45 @@ import { AuthGuard } from '../services/auth.guard';
     ReactiveFormsModule,
     FormsModule,
     CommonModule,
-    
+    NgxEditorModule.forRoot({
+      locals: {
+        // menu
+        bold: 'Bold',
+        italic: 'Italic',
+        code: 'Code',
+        underline: 'Underline',
+        strike: 'Strike',
+        blockquote: 'Blockquote',
+        bullet_list: 'Bullet List',
+        ordered_list: 'Ordered List',
+        heading: 'Heading',
+        h1: 'Header 1',
+        h2: 'Header 2',
+        h3: 'Header 3',
+        h4: 'Header 4',
+        h5: 'Header 5',
+        h6: 'Header 6',
+        align_left: 'Left Align',
+        align_center: 'Center Align',
+        align_right: 'Right Align',
+        align_justify: 'Justify',
+        text_color: 'Text Color',
+        background_color: 'Background Color',
+        insertLink: 'Insert Link',
+        removeLink: 'Remove Link',
+        insertImage: 'Insert Image',
+
+        // pupups, forms, others...
+        url: 'URL',
+        text: 'Text',
+        openInNewTab: 'Open in new tab',
+        insert: 'Insert',
+        altText: 'Alt Text',
+        title: 'Title',
+        remove: 'Remove',
+      },
+    }),
     RouterModule.forChild([
-      
       {path: '', component: AdminLayoutComponent, children:[
         {path: '', redirectTo: '/admin/login/', pathMatch: 'full'},
         {path: 'login', component: LoginPageComponent},
--- src/app/admin/components/create/create.component.ts
+++ src/app/admin/components/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Editor, Toolbar } from 'ngx-editor';
 
 @Component({
   selector: 'app-create',
@@ -7,16 +8,34 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create.component.less']
 })
 export class CreateComponent implements OnInit {
+
+  editor!: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+  html!: '';
+
   constructor(){
 
   }
   formCreateLesson!: FormGroup
 
   ngOnInit(): void {
+    this.editor = new Editor();
     this.formCreateLesson = new FormGroup({
+     text: new FormControl('', Validators.required),
       title: new FormControl(null, Validators.required)
-
     })
+  }
+  ngOnDestroy(): void {
+    this.editor.destroy();
   }
 
   submit(){
