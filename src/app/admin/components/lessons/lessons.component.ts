900d1ffac508cf147238e9ffdfe89106f9da4cdc
Ivan Kozin <Ivan.kozin@zyfra.com>
2023-04-21 09:22:10 +0300

    add page for edit lesson

--- src/app/admin/components/lessons/lessons.component.ts
+++ src/app/admin/components/lessons/lessons.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { LessonService } from 'src/app/services/lesson.service';
+import { Component, OnInit } from '@angular/core';
+import { Lesson } from 'src/app/base/interfaces/interfaces';
 
 @Component({
   selector: 'app-lessons',
   templateUrl: './lessons.component.html',
   styleUrls: ['./lessons.component.less']
 })
-export class LessonsComponent {
+export class LessonsComponent implements OnInit{
+
+  lessons: Lesson[] = []
+
+  constructor(
+    private lessonService: LessonService
+  ){
+
+  }
+  ngOnInit(): void {
+    this.lessonService.getAllLessons()
+    .subscribe((lessons) => {
+     
+      this.lessons = lessons
+      console.log(this.lessons)
+    })
+  }
 
 }
